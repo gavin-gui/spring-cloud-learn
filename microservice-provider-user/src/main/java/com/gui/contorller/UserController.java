1dce5c755dabff2e2c53266f7ddfd315a77087a2
gavin <294830054@qq.com>
2017-01-14 16:02:00 +0800

    2017-01-14 修改

--- microservice-provider-user/src/main/java/com/gui/contorller/UserController.java
+++ microservice-provider-user/src/main/java/com/gui/contorller/UserController.java
@@ -2,6 +2,7 @@ package com.gui.contorller;
 
 import com.gui.entity.User;
 import com.gui.repository.UserRepository;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by gui on 2017/1/7.
  */
 @RestController
+@Log4j
 public class UserController {
 
     @Autowired
@@ -18,6 +20,8 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable("id") Long id){
+        log.info("id:"+id);
         return userRepository.findOne(id);
+        // add by gui 2017/1/14 15:59
     }
 }
